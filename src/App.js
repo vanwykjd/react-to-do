9840f1fd39df54ddbae355464d7035d7e06b30a3
Josh Van Wyk <joshvanwyk@ymail.com>
2017-12-13 15:01:10 -0500

    Assignment 8: ToDo-Events

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
          { description: 'Throw the dishes away', isCompleted: false },
          { description: 'Buy new dishes', isCompleted: false }
        ],
-       newTodoDescription: ''
+       newTodoDescription: '',
      };
   }
   
@@ -34,12 +34,18 @@ class App extends Component {
     this.setState({ todos: todos });
   }
   
+  deleteTodo(index) {
+    const todos = this.state.todos.slice();
+    const removeTodo = todos.filter(todo => todos[index] !== todo);
+    this.setState({ todos: removeTodo});
+  }
+  
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) => 
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) } />
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,6 +6,7 @@ import React, { Component } from 'react';
        <li>
          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
+         <button onClick={ this.props.deleteTodo }>Delete</button>
        </li>
      );
    }
